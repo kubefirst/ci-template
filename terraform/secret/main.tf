c16cbb05598f181e579d3daf83fd3df5061f1321
Jessica Marinho <jessica@kubeshop.io>
2022-10-28 10:02:58 -0300

    Create AWS credentials
    Signed-off-by: Jessica Marinho <jessica@kubeshop.io>

--- terraform/secret/main.tf
+++ terraform/secret/main.tf
@@ -1,4 +1,4 @@
-resource "kubernetes_secret" "token" {
+resource "kubernetes_secret" "github_token" {
   metadata {
     name      = "github-token"
     namespace = "argo"
@@ -7,6 +7,20 @@ resource "kubernetes_secret" "token" {
   data = {
     username = "github"
     password = var.github_token
+  }
+
+  type = "kubernetes.io/basic-auth"
+}
+
+resource "kubernetes_secret" "aws_secret" {
+  metadata {
+    name      = "aws-secret"
+    namespace = "argo"
+  }
+
+  data = {
+    username = var.aws_access_key_id
+    password = var.aws_secret_access_key
   }
 
   type = "kubernetes.io/basic-auth"
