a5af1fb750d00f1c34835799ce556d8174a80f24
Cesar Filho <53096417+6za@users.noreply.github.com>
2022-11-07 09:57:58 -0300

    Create AWS credentials (#9)
    Signed-off-by: Jessica Marinho <jessica@kubeshop.io>
    
    Signed-off-by: Jessica Marinho <jessica@kubeshop.io>
    Co-authored-by: Jessica Marinho <jessica@kubeshop.io>

--- terraform/secret/variables.tf
+++ terraform/secret/variables.tf
@@ -2,3 +2,13 @@ variable "github_token" {
   type        = string
   description = "(Required) Token used to authenticate on Github."
 }
+
+variable "aws_access_key_id" {
+  type        = string
+  description = "(Required) AWS access key."
+}
+
+variable "aws_secret_access_key" {
+  type        = string
+  description = "(Required) AWS secret key."
+}
